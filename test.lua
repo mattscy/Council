4bddfb8446efec03e6a216ba65649f65e7672e8f
mattscy <35209696+mattscy@users.noreply.github.com>
2024-06-22 21:33:16 +0800

    Update test.lua

--- test.lua
+++ test.lua
@@ -10,9 +10,12 @@ frame.SizeConstraint = Enum.SizeConstraint.RelativeYY
 frame.BackgroundColor3 = Color3.fromRGB(13, 17, 23)
 frame.BorderColor3 = Color3.fromRGB(255, 255, 255)
 frame.Size = UDim2.fromScale(1, 1)
+frame.AnchorPoint = Vector2.new(0.5, 0.5)
+frame.Position = UDim2.fromScale(0.5, 0.5)
 
 local terminal = Instance.new("TextBox")
 terminal.Font = Enum.Font.Code
+terminal.TextSize = 20
 terminal.BackgroundColor3 = Color3.fromRGB(13, 17, 23)
 terminal.TextColor3 = Color3.fromRGB(255, 255, 255)
 terminal.BackgroundTransparency = 1
