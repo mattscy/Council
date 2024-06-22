77c68142f9b7eccc955eb5f6388e58eeac70e373
mattscy <35209696+mattscy@users.noreply.github.com>
2024-06-22 21:03:13 +0800

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,16 +1,26 @@
 local PLR = game:GetService("Players")
 
-local tool = Instance.new("Tool")
+local tool = script.Parent
 tool.RequiresHandle = false
 
 local sg = Instance.new("ScreenGui")
+sg.Archivable = false
+
+local frame = Instance.new("ScrollingFrame")
+frame.SizeConstraint = Enum.SizeConstraint.RelativeYY
+frame.BackgroundColor3 = Color3.fromRGB(13, 17, 23)
+frame.BorderColor3 = Color3.fromRGB(255, 255, 255)
+frame.Size = UDim2.fromScale(1, 1)
+
 local terminal = Instance.new("TextBox")
 terminal.Font = Enum.Font.Code
 terminal.BackgroundColor3 = Color3.fromRGB(13, 17, 23)
 terminal.TextColor3 = Color3.fromRGB(255, 255, 255)
-terminal.SizeConstraint = Enum.SizeConstraint.RelativeYY
-terminal.Size = UDim2.fromScale(1, 1)
-terminal.Parent = sg
+terminal.BackgroundTransparency = 1
+terminal.AutomaticSize = Enum.AutomaticSize.XY
+
+terminal.Parent = frame
+frame.Parent = sg
 
 tool.Equipped:Connect(function()
     local player = PLR:GetPlayerFromCharacter(tool.Parent)
