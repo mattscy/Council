ad2a104c538184e03d9d0e4dd4747007223a37c9
mattscy <35209696+mattscy@users.noreply.github.com>
2024-06-23 10:09:25 +0800

    Update test.lua

--- test.lua
+++ test.lua
@@ -41,6 +41,8 @@ output.Font = Enum.Font.Code
 output.TextWrapped = true
 output.Size = UDim2.new(1, 0, 0.2, 0)
 output.Position = UDim2.new(0, 0, 0.8, 0)
+output.TextXAlignment = Enum.TextXAlignment.Left
+output.TextYAlignment = Enum.TextYAlignment.Top
 output.Parent = container
 
 terminal.Parent = scroll
